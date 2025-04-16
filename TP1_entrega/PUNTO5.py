44538bd9c1e7bf0326918c05ebee9b9608f9af14
valentino_laiño <vitolaino123@gmail.com>
2025-04-15 22:16:57 -0300

    copie las consignas en el punto 5 y agregue interaciones a la consola

--- TP1_entrega/PUNTO5.py
+++ TP1_entrega/PUNTO5.py
@@ -1,3 +1,6 @@
+#Ejercicio 5: Conversor de números romanos a decimales
+#Desarrollar una función que permita convertir un número romano en un número decimal.
+
 diccionario_numromanos = {
     'I': 1,
     'V': 5,
@@ -17,5 +20,10 @@ def roman(string):
         if decimal < diccionario_numromanos[string[1]]:
             return -decimal + roman(string[1:])
     return decimal + roman(string[1:]) # Llama la cadena a partir del 2° caracter
-        
-print(roman('IV'))
+
+
+string = input("Ingrese un número romano: ") # Solicita el número romano al usuario
+print(f"El número romano es: {string}") # Imprime el número romano ingresado
+print(f"El número romano en decimal es: {roman(string)}") # Imprime el número romano convertido a decimal
+
+
